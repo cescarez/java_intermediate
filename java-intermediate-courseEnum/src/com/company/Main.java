a588ef037669d304b11e5d7af7898fbad89d11cd
Christabel Sebastian <christabel.sebastian@salesforce.com>
2021-06-02 14:14:58 -0700

    added courseEnum code task and created subdirectories

---
+++ java-intermediate-courseEnum/src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(Course.of("Math"));
+        System.out.println(Course.of("Biology"));
+        System.out.println(Course.of("Math"));
+    }
+}
